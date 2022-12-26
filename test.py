9c847ee3f77dde0762a80755468d64775ebe8588
wangyue-gagua <2503308794@qq.com>
2022-12-26 16:11:56 +0800

    忽略部分文件

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-import math
-# 3 ** n > 10 ** 7 print(n)
-for i in range(15):
-    print(3 ** i)
+import bisect
+price = [13,5,1,8,21,2]
+price.sort()
+interval = (price[-1] - price[0]) / (3 - 1)
+print(price, interval)
+print(bisect.bisect_left(price, price[0] + interval))
+print(135 / 5)
