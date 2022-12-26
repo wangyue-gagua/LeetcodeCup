8171667beb74c79d2a5cd78e45e28975fe3a7acc
wangyue-gagua <2503308794@qq.com>
2022-12-26 16:13:32 +0800

    update .gitignore

--- test.py
+++
@@ -1,7 +0,0 @@
-import bisect
-price = [13,5,1,8,21,2]
-price.sort()
-interval = (price[-1] - price[0]) / (3 - 1)
-print(price, interval)
-print(bisect.bisect_left(price, price[0] + interval))
-print(135 / 5)
