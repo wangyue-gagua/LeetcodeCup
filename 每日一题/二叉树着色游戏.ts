eda8a10476139d26ad1351ab6510ddf069051eb9
wangyue-gagua <2503308794@qq.com>
2023-02-03 12:08:53 +0800

    dfs计算节点数

---
+++ 每日一题/二叉树着色游戏.ts
@@ -0,0 +1,56 @@
+/* 有两位极客玩家参与了一场「二叉树着色」的游戏。游戏中，给出二叉树的根节点 root，树上总共有 n 个节点，且 n 为奇数，其中每个节点上的值从 1 到 n 各不相同。
+
+最开始时：
+
+「一号」玩家从 [1, n] 中取一个值 x（1 <= x <= n）；
+「二号」玩家也从 [1, n] 中取一个值 y（1 <= y <= n）且 y != x。
+「一号」玩家给值为 x 的节点染上红色，而「二号」玩家给值为 y 的节点染上蓝色。
+
+之后两位玩家轮流进行操作，「一号」玩家先手。每一回合，玩家选择一个被他染过色的节点，将所选节点一个 未着色 的邻节点（即左右子节点、或父节点）进行染色（「一号」玩家染红色，「二号」玩家染蓝色）。
+
+如果（且仅在此种情况下）当前玩家无法找到这样的节点来染色时，其回合就会被跳过。
+
+若两个玩家都没有可以染色的节点时，游戏结束。着色节点最多的那位玩家获得胜利 ✌️。
+
+现在，假设你是「二号」玩家，根据所给出的输入，假如存在一个 y 值可以确保你赢得这场游戏，则返回 true ；若无法获胜，就请返回 false 。
+
+输入：root = [1,2,3,4,5,6,7,8,9,10,11], n = 11, x = 3
+输出：true
+解释：第二个玩家可以选择值为 2 的节点。
+示例 2 ：
+
+输入：root = [1,2,3], n = 3, x = 1
+输出：false */
+
+/* *
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function btreeGameWinningMove(root: TreeNode | null, n: number, x: number): boolean {
+    let left = 0, right = 0;
+    const dfs = (root: TreeNode | null): number => {
+        if (!root) return 0;
+        const l = dfs(root.left);
+        const r = dfs(root.right);
+        if (root.val === x) {
+            // x 的左右子树节点数
+            left = l;
+            right = r;
+        }
+        return l + r + 1;
+    }
+    dfs(root);
+    // 只要x的左右子树或者x的父节点的子树节点数大于n/2，那么总有一个选择可以赢
+    return Math.max(left, right, n - left - right - 1) > n / 2;
+
+};
