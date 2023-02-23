cb016ac11a8541a83d15ab9ed6bec6ac61acf4d4
wangyue-gagua <2503308794@qq.com>
2023-02-23 12:38:15 +0800

    每日一题：格雷码，背公式

---
+++ 每日一题/循环码排列.ts
@@ -0,0 +1,29 @@
+/* 给你两个整数 n 和 start。你的任务是返回任意 (0,1,2,,...,2^n-1) 的排列 p，并且满足：
+
+p[0] = start
+p[i] 和 p[i+1] 的二进制表示形式只有一位不同
+p[0] 和 p[2^n -1] 的二进制表示形式也只有一位不同
+ 
+
+示例 1：
+
+输入：n = 2, start = 3
+输出：[3,2,0,1]
+解释：这个排列的二进制表示是 (11,10,00,01)
+     所有的相邻元素都有一位是不同的，另一个有效的排列是 [3,1,0,2]
+示例 2：
+
+输出：n = 3, start = 2
+输出：[2,6,7,5,4,0,1,3]
+解释：这个排列的二进制表示是 (010,110,111,101,100,000,001,011) */
+
+function circularPermutation(n: number, start: number): number[] {
+    const result: number[] = [];
+    for (let i = 0; i < 2 ** n; i++) {
+        result.push(start ^ i ^ (i >> 1));
+    }
+    return result;
+};
+
+console.log(circularPermutation(2, 3));
+console.log(circularPermutation(3, 2));
